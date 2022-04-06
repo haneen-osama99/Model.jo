0cbe094d4c1a7062e78e91acf669c08d98ead940
LimaAbulawi <lima.98@yahoo.com>
2022-04-06 15:29:13 +0300

    add style to home page

--- src/app/Home/home.module.ts
+++ src/app/Home/home.module.ts
@@ -1,10 +1,12 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
-
-
+import { PartnersComponent } from './partners/partners.component';
+import { ContactUsComponent } from './contact-us/contact-us.component';
+import { HomeComponent } from './home.component';
 
 @NgModule({
-  declarations: [],
+  declarations: [
+  ],
   imports: [
     CommonModule
   ]
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,18 +1,25 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { CarouselModule } from 'ngx-owl-carousel-o';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { ContactUsComponent } from './home/contact-us/contact-us.component';
 import { HomeComponent } from './home/home.component';
+import { HomeModule } from './home/home.module';
+import { PartnersComponent } from './home/partners/partners.component';
 
 @NgModule({
   declarations: [
     AppComponent,
+    PartnersComponent,
+    ContactUsComponent,
     HomeComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    CarouselModule ,
   ],
   providers: [],
   bootstrap: [AppComponent]
